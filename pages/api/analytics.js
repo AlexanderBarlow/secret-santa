8182109d3356a1818b2d3262be0a5b14efaae8ca
Alexander Barlow <alex.barlow@rocketmail.com>
2025-03-02 22:17:59 -0500

    fixed analytic dashboard

--- pages/api/analytics.js
+++ pages/api/analytics.js
@@ -1,30 +1,42 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+	if (req.method !== "GET") {
+		return res.status(405).json({ error: "Method not allowed" });
+	}
 
-  try {
-    // Fetch analytics data
-    const totalUsers = await prisma.user.count();
-    const acceptedUsers = await prisma.user.count({
-      where: { Accepted: true },
-    });
-    const usersWithWishlists = await prisma.wishlist.count();
-    const usersWithItems = await prisma.wishlist.count({
-      where: { items: { some: {} } },
-    });
+	try {
+		// Fetch analytics data for users where isAdmin is false
+		const totalUsers = await prisma.user.count({
+			where: { isAdmin: false },
+		});
 
-    // Return the analytics data
-    res.status(200).json({
-      totalUsers,
-      acceptedUsers,
-      usersWithWishlists,
-      usersWithItems,
-    });
-  } catch (error) {
-    console.error("Error fetching analytics data:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+		const acceptedUsers = await prisma.user.count({
+			where: { Accepted: true, isAdmin: false },
+		});
+
+		const usersWithWishlists = await prisma.wishlist.count({
+			where: {
+				user: { isAdmin: false },
+			},
+		});
+
+		const usersWithItems = await prisma.wishlist.count({
+			where: {
+				user: { isAdmin: false },
+				items: { some: {} },
+			},
+		});
+
+		// Return the analytics data
+		res.status(200).json({
+			totalUsers,
+			acceptedUsers,
+			usersWithWishlists,
+			usersWithItems,
+		});
+	} catch (error) {
+		console.error("Error fetching analytics data:", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 }
