40043e0988e4330e35f991ac9f12535d0f24965c
Alexander Barlow <alex.barlow@rocketmail.com>
2025-10-19 23:38:12 -0400

    ad

--- pages/_app.js
+++ pages/_app.js
@@ -14,26 +14,24 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Basic viewport for notch devices */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
-
-        {/* Manifest + Theme color */}
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1a1a40" />
 
-        {/* Android */}
+        {/* Transparent theme for Android */}
+        <meta name="theme-color" content="transparent" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="application-name" content="CFA Secret Santa" />
 
-        {/* iOS */}
+        {/* Transparent bar for iOS */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="CFA Secret Santa" />
         <link rel="apple-touch-icon" href="/candy.jpg" />
       </Head>
+
 
 
       <div className="flex flex-col min-h-[100dvh] w-full pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] bg-white">
