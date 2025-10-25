5cd1203b3ba37039a4abe4285a2432786576991d
Alexander Barlow <alex.barlow@rocketmail.com>
2025-10-25 00:12:56 -0400

    ui changes

--- pages/userdash.js
+++ pages/userdash.js
@@ -385,7 +385,7 @@ export default function UserDashboard() {
         onClick={handleLogout}
         label="Logout"
         icon={<LogOut className="w-5 h-5" />}
-        className="fixed bottom-6 right-6 z-[60] cursor-pointer"
+        className="fixed right-6 bottom-[110px] sm:bottom-6 z-[60] cursor-pointer"
       />
 
       {/* Snow Animation */}
